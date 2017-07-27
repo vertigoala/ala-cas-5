0f845f1e33f5589375bfb8ab8eaf21cadc6a52a5
Simon Bear <simon.bear@csiro.au>
2017-07-27 12:08:46 +1000

    Start of configuring Web flow

--- src/main/kotlin/au/org/ala/cas/webflow/AlaCasWebflowConfiguration.kt
+++ src/main/kotlin/au/org/ala/cas/webflow/AlaCasWebflowConfiguration.kt
@@ -1,5 +1,24 @@
 package au.org.ala.cas.webflow
 
-/**
- * Created by bea18c on 5/6/17.
- */
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.webflow.definition.registry.FlowDefinitionRegistry
+import org.springframework.webflow.engine.builder.support.FlowBuilderServices
+
+@Configuration("alaCasWebflowConfiguration")
+open class AlaCasWebflowConfiguration {
+
+    @Autowired
+    @Qualifier("loginFlowRegistry")
+    lateinit var loginFlowDefinitionRegistry: FlowDefinitionRegistry
+
+    @Autowired
+    lateinit var flowBuilderServices: FlowBuilderServices
+
+    @ConditionalOnMissingBean(name = arrayOf("authCookeWebflowConfigurer"))
+    @Bean("authCookeWebflowConfigurer")
+    open fun authCookeWebflowConfigurer(): AuthCookieWebflowConfigurer = AuthCookieWebflowConfigurer(flowBuilderServices, loginFlowDefinitionRegistry)
+}
--- src/main/kotlin/au/org/ala/cas/webflow/AuthCookieWebflowConfigurer.kt
+++ src/main/kotlin/au/org/ala/cas/webflow/AuthCookieWebflowConfigurer.kt
@@ -1,5 +1,38 @@
 package au.org.ala.cas.webflow
 
-/**
- * Created by bea18c on 5/6/17.
- */
+import au.org.ala.utils.logger
+import org.apereo.cas.authentication.principal.Response
+import org.apereo.cas.web.flow.CasWebflowConstants
+import org.apereo.cas.web.flow.DefaultWebflowConfigurer
+import org.springframework.webflow.definition.registry.FlowDefinitionRegistry
+import org.springframework.webflow.engine.Flow
+import org.springframework.webflow.engine.builder.support.FlowBuilderServices
+
+class AuthCookieWebflowConfigurer(
+        flowBuilderServices: FlowBuilderServices,
+        loginFlowDefinitionRegistry: FlowDefinitionRegistry) :
+        DefaultWebflowConfigurer(flowBuilderServices, loginFlowDefinitionRegistry) {
+
+    companion object {
+        val log = logger<AuthCookieWebflowConfigurer>()
+    }
+
+    override fun doInitialize() {
+        log.info("doInitialize()")
+        createRedirectToServiceActionState(loginFlow)
+    }
+
+    /**
+     * Create redirect to service action state.
+
+     * @param flow the flow
+     */
+    override protected fun createRedirectToServiceActionState(flow: Flow) {
+        val redirectToView = createActionState(flow,
+                CasWebflowConstants.STATE_ID_REDIRECT,
+                createEvaluateAction("redirectToServiceAction"))
+        createTransitionForState(redirectToView, Response.ResponseType.POST.name.toLowerCase(), CasWebflowConstants.STATE_ID_POST_VIEW)
+        createTransitionForState(redirectToView, Response.ResponseType.REDIRECT.name.toLowerCase(), CasWebflowConstants.STATE_ID_REDIR_VIEW)
+    }
+
+}
