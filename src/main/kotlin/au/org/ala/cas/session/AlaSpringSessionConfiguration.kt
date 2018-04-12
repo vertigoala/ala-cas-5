6f16149ab7ec0a6f5ca967291bcba5acbb71c3dd
Simon Bear <simon.bear@csiro.au>
2018-04-12 18:38:50 +1000

    Enable Redis auto config for Spring Session

--- src/main/kotlin/au/org/ala/cas/session/AlaSpringSessionConfiguration.kt
+++ src/main/kotlin/au/org/ala/cas/session/AlaSpringSessionConfiguration.kt
@@ -1,16 +1,17 @@
 package au.org.ala.cas.session
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
 import org.springframework.session.data.redis.config.ConfigureRedisAction
 
 @Configuration
+@Import(RedisAutoConfiguration::class) // Need to import manually because @SpringBootApplication on CasWebApplication::class excludes it
 class AlaSpringSessionConfiguration {
 
     @ConditionalOnProperty("spring.session.disable-redis-config-action")
     @Bean
-    fun configureRedisAction(): ConfigureRedisAction {
-        return ConfigureRedisAction.NO_OP
-    }
+    fun configureRedisAction(): ConfigureRedisAction = ConfigureRedisAction.NO_OP
 }
