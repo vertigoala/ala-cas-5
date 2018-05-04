f254567d9ad05b96be7a4d591019e6f3cc2e95de
Simon Bear <simon.bear@csiro.au>
2018-05-05 00:54:18 +1000

    - Don't crash in GenerateAuthCookieAction if TGT is null or blank  - Use email attribute in preference to id for auth cookie value for delegated auth (which no longer has email as id)

--- src/main/kotlin/au/org/ala/cas/webflow/GenerateAuthCookieAction.kt
+++ src/main/kotlin/au/org/ala/cas/webflow/GenerateAuthCookieAction.kt
@@ -26,16 +26,20 @@ open class GenerateAuthCookieAction(
         // Create ALA specific cookie that any ALA web application can read
         //
         val ticketGrantingTicket = WebUtils.getTicketGrantingTicketId(context)
-        log.debug("Ticket-granting ticket found in the context is [{}]", ticketGrantingTicket)
 
-        val authentication =
-            this.ticketRegistrySupport.getAuthenticationFrom(ticketGrantingTicket) ?: throw InvalidTicketException(
-                AuthenticationException("No authentication found for ticket " + ticketGrantingTicket),
-                ticketGrantingTicket
-            )
-        val email = authentication.principal.id
+        if (!ticketGrantingTicket.isNullOrBlank()) {
+            log.debug("Ticket-granting ticket found in the context is [{}]", ticketGrantingTicket)
+            val authentication =
+                this.ticketRegistrySupport.getAuthenticationFrom(ticketGrantingTicket) ?: throw InvalidTicketException(
+                    AuthenticationException("No authentication found for ticket $ticketGrantingTicket"),
+                    ticketGrantingTicket
+                )
+            val email = authentication.principal.attributes["email"] ?: authentication.principal.id
 
-        alaProxyAuthenticationCookieGenerator.addCookie(context, email)
+            alaProxyAuthenticationCookieGenerator.addCookie(context, email.toString())
+        } else {
+            log.debug("Ticket-granting ticket ID is blank")
+        }
 
         return success()
     }
