4915bac0c27ca6e9e3ed0222128b97fb80100f62
Andre Fernandes <andre@vertigo.com.br>
2019-08-17 19:38:59 -0300

    cas + userdetails

--- setenv.sh
+++ setenv.sh
@@ -7,9 +7,10 @@
 #export DOCKER_HOST=tcp://$SERVERNAME:2375
 #export DOCKER_HOST=ssh://ubuntu@$SERVERNAME
 #export ALA_URL=http://$SERVERNAME # traefik port
-export CAS_SERVER=bf6a657b.ngrok.io
-export CAS_DOMAIN=ngrok.io
+export CAS_SERVER=crapula.serveo.net
+export CAS_DOMAIN=serveo.net
 export ALA_URL=https://$CAS_SERVER # traefik port
 sed "s#\$ALA_URL#$ALA_URL#g" ./config/application-template.yml | \
  sed "s#\$CAS_DOMAIN#$CAS_DOMAIN#g" | \
  sed "s#\$CAS_SERVER#$CAS_SERVER#g" > ./config/application.yml
+sed "s#\$ALA_URL#$ALA_URL#g" ./config/userdetails-config-template.properties > ./config/userdetails-config.properties
